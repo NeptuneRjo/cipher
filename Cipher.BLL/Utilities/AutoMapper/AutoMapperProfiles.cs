b04b1793a661a711676bb0e98b99ced9530a2ed1
NeptuneRjo <rjo9000@outlook.com>
2023-09-24 14:02:56 -0400

    Create file for auto mapper profiles

---
+++ Cipher.BLL/Utilities/AutoMapper/AutoMapperProfiles.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Cipher.BLL.Utilities.AutoMapper
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            
+        }
+    }
+}
