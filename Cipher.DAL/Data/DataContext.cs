9e2d6c866630d259b306ad1e1176aa87175e0792
NeptuneRjo <rjo9000@outlook.com>
2023-09-24 13:49:40 -0400

    Scaffold db context

---
+++ Cipher.DAL/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cipher.DAL.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() { }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        
+        }
+    }
+}
