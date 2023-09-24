85dcab7bdc07f5ff041b8a3177efa6d7a834fc42
NeptuneRjo <rjo9000@outlook.com>
2023-09-24 04:52:45 -0400

    scaffold dependency injection and install required packages

---
+++ Cipher.BLL/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cipher.BLL
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterBLLDependencies(this IServiceCollection services, IConfiguration Configuration)
+        {
+
+        }
+    }
+}
---
+++ Cipher.DAL/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cipher.DAL
+{
+    public static class DependencyInjection
+    {
+        public static void RegisterDALDependencies(this IServiceCollection services, IConfiguration Configuration)
+        {
+
+        }
+    }
+}
