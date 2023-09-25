66d973f2699a786cf145d046dfbb19d9c92d4406
NeptuneRjo <rjo9000@outlook.com>
2023-09-25 00:40:53 -0400

    Add entities to data context

--- Cipher.DAL/Data/DataContext.cs
+++ Cipher.DAL/Data/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Cipher.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cipher.DAL.Data
 {
@@ -8,10 +9,19 @@ namespace Cipher.DAL.Data
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
+        DbSet<Chat> Chats { get; set; }
+
+        DbSet<Message> Messages { get; set; }
+
+        DbSet<User> Users { get; set; }
+
+        DbSet<ChatUser> ChatUsers { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-        
+            modelBuilder.Entity<ChatUser>()
+                .HasKey(cu => new { cu.ChatId, cu.UserId });
         }
     }
 }
