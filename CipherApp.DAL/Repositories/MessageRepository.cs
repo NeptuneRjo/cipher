89379d8fbcf3b75902a097d3a76eb9c3eafb825c
NeptuneRjo <rjo9000@outlook.com>
2023-09-25 05:18:59 -0400

    Create scaffolding for repository implementations

---
+++ CipherApp.DAL/Repositories/MessageRepository.cs
@@ -0,0 +1,16 @@
+﻿using CipherApp.DAL.Data;
+using CipherApp.DAL.Entities;
+using CipherApp.DAL.Repositories.IRepositories;
+
+namespace CipherApp.DAL.Repositories
+{
+    public class MessageRepository : GenericRepository<Message>, IMessageRepository
+    {
+        private readonly DataContext _context;
+
+        public MessageRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ CipherApp.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using CipherApp.DAL.Data;
+using CipherApp.DAL.Entities;
+using CipherApp.DAL.Repositories.IRepositories;
+
+namespace CipherApp.DAL.Repositories
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
