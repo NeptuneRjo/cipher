ee4837dd2c9ea8e2d3b626e78fed94ecd3047153
NeptuneRjo <rjo9000@outlook.com>
2023-09-25 17:26:09 -0400

    Add repositories to scope

--- CipherApp.DAL/DependencyInjection.cs
+++ CipherApp.DAL/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using CipherApp.DAL.Data;
+using CipherApp.DAL.Repositories;
+using CipherApp.DAL.Repositories.IRepositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,6 +11,10 @@ namespace CipherApp.DAL
     {
         public static void RegisterDALDependencies(this IServiceCollection services, IConfiguration Configuration)
         {
+            services.AddScoped<IChatRepository, ChatRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
             services.AddDbContext<DataContext>(options =>
             {
                 var defaultConnectionString = Configuration.GetConnectionString("DefaultConnection");
