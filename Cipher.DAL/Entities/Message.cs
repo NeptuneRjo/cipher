6c32bc8348209a78b29d7bfd5621a04adc4bfc77
NeptuneRjo <rjo9000@outlook.com>
2023-09-25 00:40:39 -0400

    Create message entity

---
+++ Cipher.DAL/Entities/Message.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Cipher.DAL.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int UserId { get; set; }
+        public int ChatId { get; set; }
+        public string Username { get; set; }
+    }
+}
