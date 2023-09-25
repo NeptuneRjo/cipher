a3b5fd1067a09e1932a7d708cb2d6b2c649ac73b
NeptuneRjo <rjo9000@outlook.com>
2023-09-25 00:40:22 -0400

    Create chat entity

---
+++ Cipher.DAL/Entities/Chat.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Cipher.DAL.Entities
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int OwnerId { get; set; }
+        public DateTime LastMessage { get; set; }
+    }
+}
