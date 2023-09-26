878167071ce923592140eb3ebf509810f6cacd92
NeptuneRjo <rjo9000@outlook.com>
2023-09-26 17:57:31 -0400

    Scaffold implementation of services layer

---
+++ CipherApp.BLL/Services/ChatService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CipherApp.BLL.Operations.IOperations;
+using CipherApp.BLL.Services.IServices;
+
+namespace CipherApp.BLL.Services
+{
+    public class ChatService : IChatService
+    {
+        private readonly IMapper _mapper;
+        private readonly IChatOperations _ops;
+
+        public ChatService(IMapper mapper, IChatOperations ops)
+        {
+            _mapper = mapper;
+            _ops = ops;
+        }
+
+    }
+}
---
+++ CipherApp.BLL/Services/MessageService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CipherApp.BLL.Operations.IOperations;
+using CipherApp.BLL.Services.IServices;
+
+namespace CipherApp.BLL.Services
+{
+    public class MessageService : IMessageService
+    {
+        private readonly IMessageOperations _ops;
+        private readonly IMapper _mapper;
+
+        public MessageService(IMessageOperations ops, IMapper mapper)
+        {
+            _mapper = mapper;
+            _ops = ops;
+        }
+
+    }
+}
