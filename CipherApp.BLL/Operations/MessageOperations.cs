db2354649796f4c6125bb67799fe911944dbf8cc
NeptuneRjo <rjo9000@outlook.com>
2023-09-26 17:57:12 -0400

    Scaffold implementation of operations layer

---
+++ CipherApp.BLL/Operations/MessageOperations.cs
@@ -0,0 +1,19 @@
+﻿using CipherApp.BLL.Operations.IOperations;
+using CipherApp.DAL.Repositories.IRepositories;
+using Microsoft.Extensions.Logging;
+
+namespace CipherApp.BLL.Operations
+{
+    public class MessageOperations : IMessageOperations
+    {
+        private readonly IMessageRepository _repository;
+        private readonly ILogger<MessageOperations> _logger;
+
+        public MessageOperations(IMessageRepository repository, ILogger<MessageOperations> logger)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+    }
+}
