910887c978d6eb10b55a07e050d24dfffbde8bbe
NeptuneRjo <rjo9000@outlook.com>
2023-09-29 20:10:25 -0400

    Configure authentication in BLL

--- CipherApp.BLL/DependencyInjection.cs
+++ CipherApp.BLL/DependencyInjection.cs
@@ -1,6 +1,11 @@
 ﻿using Cipher.BLL.Utilities.AutoMapper;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Cipher.BLL
 {
@@ -9,6 +14,38 @@ namespace Cipher.BLL
         public static void RegisterBLLDependencies(this IServiceCollection services, IConfiguration Configuration)
         {
             services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddCookie()
+                .AddJwtBearer(options =>
+                {
+                    SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]));
+
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                })
+                .AddGitHub(options =>
+                {
+                    options.ClientId = Configuration["GitHub:ClientId"];
+                    options.ClientSecret = Configuration["GitHub:ClientSecret"];
+                });
+
+            services.AddLogging(builder =>
+            {
+                builder.AddConsole();
+            });
         }
     }
 }
