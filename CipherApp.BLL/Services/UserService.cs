fa352d695428ade846579c367d2138d71dcab0a0
NeptuneRjo <rjo9000@outlook.com>
2023-10-02 20:35:59 -0400

    Define user-authentication methods

--- CipherApp.BLL/Services/UserService.cs
+++ CipherApp.BLL/Services/UserService.cs
@@ -3,6 +3,7 @@ using CipherApp.BLL.Services.IServices;
 using CipherApp.BLL.Utilities.CustomExceptions;
 using CipherApp.DAL.Entities;
 using CipherApp.DAL.Repositories.IRepositories;
+using CipherApp.DTO.Request;
 using CipherApp.DTO.Response;
 using Microsoft.Extensions.Logging;
 using System.Linq.Expressions;
@@ -30,21 +31,58 @@ namespace CipherApp.BLL.Services
             _mapper = mapper;
         }
 
-        public async Task<UserDto> GetUserAsync(string uid)
+        public async Task<UserDto> AuthUserAsync(string username, string password)
         {
-            _logger.LogInformation($"User requested with uid = {uid}");
-            var user = await _repository.GetByQueryAsync(e => e.UID == uid, includes);
+            var user = await GetUserByUsername(username);
+
+            if (user.ValidatePassword(password))
+                return _mapper.Map<UserDto>(user);
+
+            throw new UnauthorizedAccessException();
+        }
+
+        public async Task<UserDto> AuthUserAsync(int id, string password)
+        {
+            var user = await GetUserById(id);
+
+            if (user.ValidatePassword(password))
+                return _mapper.Map<UserDto>(user);
+
+            throw new UnauthorizedAccessException();
+        }
+
+        public async Task<UserDto> CreateUserAsync(NewUserDto newUser)
+        {
+            var user = _mapper.Map<User>(newUser);
+
+            user.EncryptPassword();
+
+            var added = await _repository.AddAsync(user);
+
+            return _mapper.Map<UserDto>(added);
+        }
+
+        public async Task<UserDto> GetUserAsync(string username) =>
+            _mapper.Map<UserDto>(await GetUserByUsername(username));
+
+        public async Task<UserDto> GetUserAsync(int id) =>
+            _mapper.Map<UserDto>(await GetUserById(id));
+
+        private async Task<User> GetUserByUsername(string username)
+        {
+            _logger.LogInformation($"User requested with username = {username}");
+            var user = await _repository.GetByQueryAsync(e => e.Username == username, includes);
 
             if (user == null)
             {
-                _logger.LogError($"User with the uid = {uid} was not found");
+                _logger.LogError($"User with the username = {username} was not found");
                 throw new NotFoundException();
             }
 
-            return _mapper.Map<UserDto>(user);
+            return user;
         }
 
-        public async Task<UserDto> GetUserAsync(int id)
+        private async Task<User> GetUserById(int id)
         {
             _logger.LogInformation($"User requested with id = {id}");
             var user = await _repository.GetByQueryAsync(e => e.Id == id, includes);
@@ -55,7 +93,7 @@ namespace CipherApp.BLL.Services
                 throw new NotFoundException();
             }
 
-            return _mapper.Map<UserDto>(user);
+            return user;
         }
     }
 }
