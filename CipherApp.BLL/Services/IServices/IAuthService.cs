45b69a7f0728c6cfc283c3e875e49ce06f01d790
NeptuneRjo <rjo9000@outlook.com>
2023-10-14 02:25:07 -0400

    Define authentication service interface

---
+++ CipherApp.BLL/Services/IServices/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using CipherApp.DAL.Entities;
+using CipherApp.DTO.Request;
+using CipherApp.DTO.Response;
+
+namespace CipherApp.BLL.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<UserDto> LoginAsync(UserToLoginDto userToLoginDto);
+
+        Task<User> RegisterAsync(NewUserDto userToRegisterDto);
+    }
+}
