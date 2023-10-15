145efd53cffb2d59809603f9aa89d9ae33d39300
NeptuneRjo <rjo9000@outlook.com>
2023-10-15 03:30:19 -0400

    Create custom exception for validation failure

---
+++ CipherApp.BLL/Utilities/CustomExceptions/LoginFailedException.cs
@@ -0,0 +1,20 @@
+﻿namespace CipherApp.BLL.Utilities.CustomExceptions
+{
+    public class LoginFailedException : Exception
+    {
+        public LoginFailedException()
+        {
+            
+        }
+
+        public LoginFailedException(string message) : base(message)
+        {
+            
+        }
+
+        public LoginFailedException(string message, Exception inner) : base(message, inner) 
+        { 
+        
+        }
+    }
+}
