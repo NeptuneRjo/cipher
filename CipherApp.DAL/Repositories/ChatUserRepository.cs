03af2e04fe7459e53dbe832d77e36b263214e556
NeptuneRjo <rjo9000@outlook.com>
2023-10-17 17:08:34 -0400

    Create repository for chatuser join table

---
+++ CipherApp.DAL/Repositories/ChatUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using CipherApp.DAL.Data;
+using CipherApp.DAL.Entities;
+using CipherApp.DAL.Repositories.IRepositories;
+
+namespace CipherApp.DAL.Repositories
+{
+    public class ChatUserRepository : GenericRepository<ChatUser>, IChatUserRepository
+    {
+        private readonly DataContext _context;
+
+        public ChatUserRepository(DataContext context) : base(context)
+        {
+            _context = context;            
+        }
+
+    }
+}
---
+++ CipherApp.DAL/Repositories/IRepositories/IChatUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using CipherApp.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherApp.DAL.Repositories.IRepositories
+{
+    public interface IChatUserRepository : IGenericRepository<ChatUser>
+    {
+    }
+}
