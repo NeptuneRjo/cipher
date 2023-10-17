bd5b49dd9f248cee014eeb5e855f451286ff523a
NeptuneRjo <rjo9000@outlook.com>
2023-10-17 17:08:44 -0400

    Define request dto for creating chat

---
+++ CipherApp.DTO/Request/ChatToCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherApp.DTO.Request
+{
+    public class ChatToCreateDto
+    {
+        public string? Name { get; set; }
+        public int? OwnerId { get; set; }
+    }
+}
