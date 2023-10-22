4d6f7691e7da74ed96254e7d819fbbfe785a8c9a
NeptuneRjo <rjo9000@outlook.com>
2023-10-22 02:59:58 -0400

    Remove code that falls outside of SRP

--- CipherApp.API/Pages/Register.cshtml.cs
+++ CipherApp.API/Pages/Register.cshtml.cs
@@ -1,13 +1,10 @@
+using CipherApp.API.Utilities;
 using CipherApp.BLL.Services.IServices;
 using CipherApp.BLL.Utilities.CustomExceptions;
-using CipherApp.DAL.Entities;
-using CipherApp.DTO.Request;
-using Microsoft.AspNetCore.Authentication;
+using CipherApp.DAL.Models;
+using CipherApp.DTO.Response;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
 
 namespace CipherApp.API.Pages
 {
@@ -20,20 +17,8 @@ namespace CipherApp.API.Pages
             _service = service;
         }
 
-        public class InputModel
-        {
-            [Required, Display(Name = "Username")]
-            public string Username { get; set; }
-            
-            [Required, DataType(DataType.Password), Display(Name = "Password one")]
-            public string Password1 { get; set; }
-
-            [Required, DataType(DataType.Password), Display(Name = "Password two")]
-            public string Password2 { get; set; }
-        }
-
         [BindProperty]
-        public InputModel Input { get; set; }
+        public RegisterInputModel Input { get; set; }
 
         public void OnGet()
         {
@@ -44,13 +29,14 @@ namespace CipherApp.API.Pages
             if (!ModelState.IsValid)
                 return Page();
 
-            if (VerifyPasswords(Input))
+            if (Input.Password == Input.Password2)
             {
                 try
                 {
-                    User user = await _service.RegisterAsync(MapInput(Input));
+                    UserDto user = await _service.RegisterAsync(Input);
 
-                    await HandleAuthentication(user.Id, user.Username);
+                    await AuthenticationHandler
+                        .Authenticate(HttpContext, user.Id, user.Username);
 
                     return RedirectToPage("/Index");
                 }
@@ -62,41 +48,6 @@ namespace CipherApp.API.Pages
             ModelState.AddModelError("Password", "Passwords do not match");
 
             return Page();
-        }
-
-        private static bool VerifyPasswords(InputModel input) =>
-            input.Password1 == input.Password2;
-
-        private UserToRegisterDto MapInput(InputModel input)
-        {
-            UserToRegisterDto dto = new()
-            {
-                Username = Input.Username,
-                Password = Input.Password1,
-            };
-
-            return dto;
-        }
-
-        private async Task HandleAuthentication(int userId, string username)
-        {
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
-                new Claim(ClaimTypes.Name, username)
-            };
-
-            var claimsIdentity = new ClaimsIdentity(claims, "CookieAuthentication");
-            var authProperties = new AuthenticationProperties
-            {
-                IsPersistent = true,
-                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(60 * 12)
-            };
-
-            await HttpContext.SignInAsync(
-                "CookieAuthentication", 
-                new ClaimsPrincipal(claimsIdentity), 
-                authProperties);
         }
     }
 }
