f689afdd84499095ed86487c27f129b30f9bcfa2
NeptuneRjo <rjo9000@outlook.com>
2023-10-22 02:59:40 -0400

    Remove code that falls outside of SRP

--- CipherApp.API/Pages/Login.cshtml.cs
+++ CipherApp.API/Pages/Login.cshtml.cs
@@ -1,13 +1,10 @@
+using CipherApp.API.Utilities;
 using CipherApp.BLL.Services.IServices;
 using CipherApp.BLL.Utilities.CustomExceptions;
-using CipherApp.DAL.Entities;
-using CipherApp.DTO.Request;
+using CipherApp.DAL.Models;
 using CipherApp.DTO.Response;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
 
 namespace CipherApp.API.Pages
 {
@@ -20,17 +17,8 @@ namespace CipherApp.API.Pages
             _service = service;
         }
 
-        public class InputModel
-        {
-            [Required, Display(Name = "Username")]
-            public string Username { get; set; }
-
-            [Required, DataType(DataType.Password), Display(Name = "Password")]
-            public string Password { get; set; }
-        }
-
         [BindProperty]
-        public InputModel Input { get; set; }
+        public LoginInputModel Input { get; set; }
 
         public void OnGet()
         {
@@ -43,9 +31,10 @@ namespace CipherApp.API.Pages
 
             try
             {
-                UserDto user = await _service.LoginAsync(MapInput());
+                UserDto user = await _service.LoginAsync(Input);
 
-                await HandleAuthentication(user.Id, user.Username);
+                await AuthenticationHandler
+                    .Authenticate(HttpContext, user.Id, user.Username);
 
                 return RedirectToPage("/Index");
             }
@@ -59,38 +48,6 @@ namespace CipherApp.API.Pages
             }
 
             return Page();
-        }
-
-        private UserToLoginDto MapInput()
-        {
-            UserToLoginDto toLoginDto = new()
-            {
-                username = Input.Username,
-                password = Input.Password,
-            };
-
-            return toLoginDto;
-        }
-
-        private async Task HandleAuthentication(int userId, string username)
-        {
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
-                new Claim(ClaimTypes.Name, username)
-            };
-
-            var claimsIdentity = new ClaimsIdentity(claims, "CookieAuthentication");
-            var authProperties = new AuthenticationProperties
-            {
-                IsPersistent = true,
-                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(60 * 12)
-            };
-
-            await HttpContext.SignInAsync(
-                "CookieAuthentication",
-                new ClaimsPrincipal(claimsIdentity),
-                authProperties);
         }
     }
 }
---
+++ CipherApp.API/Utilities/AuthenticationHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace CipherApp.API.Utilities
+{
+    public static class AuthenticationHandler
+    {
+        public static async Task Authenticate(HttpContext context, int userId, string username)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, "CookieAuthentication");
+            var authProperties = new AuthenticationProperties
+            {
+                IsPersistent = true,
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(60 * 12)
+            };
+
+            await context.SignInAsync(
+                "CookieAuthentication",
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+        }
+    }
+}
