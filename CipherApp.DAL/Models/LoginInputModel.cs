1b1bac5f9c75357bdacda42beecd9d21432bf7fa
NeptuneRjo <rjo9000@outlook.com>
2023-10-22 01:57:53 -0400

    Create models for razor pages

---
+++ CipherApp.DAL/Models/LoginInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CipherApp.DAL.Models
+{
+    public class LoginInputModel
+    {
+        [Required, Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password), Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
---
+++ CipherApp.DAL/Models/RegisterInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherApp.DAL.Models
+{
+    public class RegisterInputModel
+    {
+        [Required, Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password), Display(Name = "Password one")]
+        public string Password { get; set; }
+
+        [Required, DataType(DataType.Password), Display(Name = "Password two")]
+        public string Password2 { get; set; }
+    }
+}
