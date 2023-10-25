afcf19cce7ca9962610576724de98b0d96d31f18
NeptuneRjo <rjo9000@outlook.com>
2023-10-25 04:51:46 -0400

    Create join table for chat messages

---
+++ CipherApp.DAL/Entities/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace CipherApp.DAL.Entities
+{
+    public class ChatMessage
+    {
+        public int Id { get; set; }
+        public int ChatId { get; set; }
+        public int SenderId { get; set; }
+        public string Content { get; set; }
+        public DateTime SentAt { get; set; }
+
+        public Chat Chat { get; set; }
+        public User Sender { get; set; }
+    }
+}
