b858be088fa726173d9a231c47b6e3f356d7d0e4
NeptuneRjo <rjo9000@outlook.com>
2023-10-25 04:51:05 -0400

    Update chat

--- CipherApp.DAL/Entities/Chat.cs
+++ CipherApp.DAL/Entities/Chat.cs
@@ -3,11 +3,11 @@
     public class Chat
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
-        public int OwnerId { get; set; }
-        public DateTime LastMessage { get; set; }
+        public int ParticipantOneId { get; set; }
+        public int ParticipantTwoId { get; set; }
 
-        public ICollection<ChatUser> ChatUsers { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastMessage { get; set; }
 
         public ICollection<Message> Messages { get; set; }
     }
