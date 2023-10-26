3314a1b291c72b02711eb913a28d3b90eec16501
NeptuneRjo <rjo9000@outlook.com>
2023-10-25 22:47:11 -0400

    Update dto

--- CipherApp.DTO/Response/ChatDto.cs
+++ CipherApp.DTO/Response/ChatDto.cs
@@ -4,11 +4,11 @@ namespace CipherApp.DTO.Response
     public class ChatDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int OwnerId { get; set; }
-        public DateTime LastMessage { get; set; }
+        public int ParticipantOneId { get; set; }
+        public int ParticipantTwoId { get; set; }
 
-        public ICollection<UserDto> Users { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastMessage { get; set; }
 
         public ICollection<MessageDto> Messages { get; set; }
     }
