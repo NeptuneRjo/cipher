aa58e6ce75cc2d552e293bfb4c5dcf86e6ea6f9c
NeptuneRjo <rjo9000@outlook.com>
2023-11-05 01:07:19 -0500

    Update login input model to use email instead of username

--- CipherApp.DAL/Models/LoginInputModel.cs
+++ CipherApp.DAL/Models/LoginInputModel.cs
@@ -4,8 +4,8 @@ namespace CipherApp.DAL.Models
 {
     public class LoginInputModel
     {
-        [Required, Display(Name = "Username")]
-        public string Username { get; set; }
+        [Required, Display(Name = "Email")]
+        public string Email { get; set; }
 
         [Required, DataType(DataType.Password), Display(Name = "Password")]
         public string Password { get; set; }
