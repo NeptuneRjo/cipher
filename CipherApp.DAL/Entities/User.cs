6d5de7a0ca4554a8880b8dd22535237fd9d7dcc1
NeptuneRjo <rjo9000@outlook.com>
2023-11-05 01:07:00 -0500

    Update user model

--- CipherApp.DAL/Entities/User.cs
+++ CipherApp.DAL/Entities/User.cs
@@ -1,13 +1,24 @@
 ﻿namespace CipherApp.DAL.Entities
 {
-    public class User
+    using BCrypt.Net;
+
+    public partial class User
     {
         public int Id { get; set; }
         public string Username { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
 
         public ICollection<Message> Messages { get; set; }
 
         public int ChatId { get; set; }
         public Chat Chat { get; set; }
+    }
+
+    public partial class User 
+    { 
+        public bool ValidatePassword(string password) =>
+            BCrypt.Verify(password, Password);
     }
 }
