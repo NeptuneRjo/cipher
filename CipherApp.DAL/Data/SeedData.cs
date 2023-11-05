0ec24a392fd29093e08ffd116ce2676aa5d7acad
NeptuneRjo <rjo9000@outlook.com>
2023-11-05 01:06:51 -0500

    Update seed class

--- CipherApp.DAL/Data/SeedData.cs
+++ CipherApp.DAL/Data/SeedData.cs
@@ -30,13 +30,11 @@ namespace CipherApp.DAL.Data
                                 new Message()
                                 {
                                     Content = "test",
-                                    Username = "user1",
                                     ChatId = 1,
                                 },
                                 new Message()
                                 {
                                     Content = "Hello world",
-                                    Username = "user2",
                                     ChatId = 1
                                 }
                             },
@@ -44,11 +42,13 @@ namespace CipherApp.DAL.Data
                             {
                                 new User()
                                 {
+                                    Email = "test@email.com",
                                     Username = "test",
                                     ChatId = 1,
                                 },
                                 new User()
                                 {
+                                    Email = "email@email.com",
                                     Username = "username",
                                     ChatId = 1,
                                 }
@@ -62,33 +62,5 @@ namespace CipherApp.DAL.Data
                 return;
             }
         }
-
-        private static void SeedUsers(DataContext context)
-        {
-            context.Users.AddRange(
-                new User
-                {
-                    Id = 1,
-                    Username = "username",
-                    Messages = new List<Message>(),
-                    ChatId = 1,
-                },
-                new User
-                {
-                    Id = 2,
-                    Username = "test",
-                    Messages = new List<Message>(),
-                    ChatId = 2,
-                }
-            );
-
-            context.SaveChanges();
-        }
-
-        private static void SeedChats(DataContext context) 
-        {
-            
-        }
-
     }
 }
