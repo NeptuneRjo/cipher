8a37a84883e0f51fbbfe1eb9a7a1358ad526210e
NeptuneRjo <rjo9000@outlook.com>
2023-11-06 00:14:45 -0500

    Add register page

---
+++ CipherApp.API/Pages/Register.cshtml.cs
@@ -0,0 +1,59 @@
+using CipherApp.API.Utilities;
+using CipherApp.BLL.Services.IServices;
+using CipherApp.BLL.Utilities.CustomExceptions;
+using CipherApp.DAL.Models;
+using CipherApp.DTO.Response;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CipherApp.API.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly IAuthService _service;
+
+        [BindProperty]
+        public RegisterInputModel RegisterInput { get; set; }
+
+        public RegisterModel(IAuthService service)
+        {
+            _service = service;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            try
+            {
+                UserDto user = await _service.RegisterAsync(RegisterInput);
+
+                await AuthenticationHandler.Authenticate(
+                    HttpContext,
+                    user.Id,
+                    user.Username
+                );
+
+                return RedirectToPage("./Index");
+            }
+            catch (UserExistsException)
+            {
+                ModelState.AddModelError("RegisterError", "Email already in use");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("ApplicationError", "Something went wrong");
+            }
+
+            return Page();
+        }
+
+        private bool PasswordsValid() =>
+            RegisterInput.Password == RegisterInput.ConfirmPassword;
+    }
+}
