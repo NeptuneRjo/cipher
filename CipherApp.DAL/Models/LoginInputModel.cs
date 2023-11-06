a299a4b548bcf75e743f8aa5c973da7693992809
NeptuneRjo <rjo9000@outlook.com>
2023-11-06 00:30:26 -0500

    Add email validation to login model

--- CipherApp.DAL/Models/LoginInputModel.cs
+++ CipherApp.DAL/Models/LoginInputModel.cs
@@ -5,6 +5,7 @@ namespace CipherApp.DAL.Models
     public class LoginInputModel
     {
         [Required, Display(Name = "Email")]
+        [RegularExpression(@"^[\w-_]+(\.[\w!#$%'*+\/=?\^`{|}]+)*@((([\-\w]+\.)+[a-zA-Z]{2,20})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$", ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
 
         [Required, DataType(DataType.Password), Display(Name = "Password")]
