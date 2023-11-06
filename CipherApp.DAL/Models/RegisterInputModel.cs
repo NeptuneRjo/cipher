df81d4be9947852812d7a1a3214858092ff79c60
NeptuneRjo <rjo9000@outlook.com>
2023-11-06 00:15:26 -0500

    Add necessary fields and validation to register input model

--- CipherApp.DAL/Models/RegisterInputModel.cs
+++ CipherApp.DAL/Models/RegisterInputModel.cs
@@ -12,10 +12,15 @@ namespace CipherApp.DAL.Models
         [Required, Display(Name = "Username")]
         public string Username { get; set; }
 
-        [Required, DataType(DataType.Password), Display(Name = "Password one")]
+        [Required, Display(Name = "Email")]
+        [RegularExpression(@"^[\w-_]+(\.[\w!#$%'*+\/=?\^`{|}]+)*@((([\-\w]+\.)+[a-zA-Z]{2,20})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password), Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Required, DataType(DataType.Password), Display(Name = "Password two")]
-        public string Password2 { get; set; }
+        [Required, DataType(DataType.Password), Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
     }
 }
