9be473a725777819e9544197f6beea96e55c1425
NeptuneRjo <rjo9000@outlook.com>
2023-11-15 01:44:52 -0500

    Create signalr hub for chat

---
+++ CipherApp.API/Hubs/ChatHub.cs
@@ -0,0 +1,47 @@
+﻿using CipherApp.BLL.Services.IServices;
+using CipherApp.DAL.Models;
+using CipherApp.DTO.Response;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CipherApp.API.Hubs
+{
+    public interface IChatHub
+    {
+        Task JoinGroup(string chatUID);
+
+        Task SendMessage(string UID, string content, int userId);
+    }
+
+    [Authorize]
+    public class ChatHub : Hub, IChatHub
+    {
+        private readonly IMessageService _msgService;
+
+        public ChatHub(IMessageService msgService)
+        {
+            _msgService = msgService;
+        }
+
+        public async Task SendMessage(string UID, string content, int userID)
+        {
+            try
+            {
+                MessageDto message = await _msgService.AddMessageAsync(UID, content, userID);
+
+                await Clients.Group(UID).SendAsync("ReceiveMessage", message);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+        }
+
+        public async Task JoinGroup(string chatUID) =>
+            await Groups.AddToGroupAsync(Context.ConnectionId, chatUID);
+
+    }
+}
