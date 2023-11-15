f57b2c04bafe9d5b72ed6511b5a42bdecf280364
NeptuneRjo <rjo9000@outlook.com>
2023-11-15 01:49:41 -0500

    Create method for adding message

--- CipherApp.DAL/Repositories/IRepositories/IMessageRepository.cs
+++ CipherApp.DAL/Repositories/IRepositories/IMessageRepository.cs
@@ -4,5 +4,6 @@ namespace CipherApp.DAL.Repositories.IRepositories
 {
     public interface IMessageRepository : IGenericRepository<Message>
     {
+        Task<Message> CreateAndAddToChatAsync(string chatUID, string content, int userId);
     }
 }
