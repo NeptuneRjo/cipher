38524492589483be0e9e47c8d60efc148529de58
NeptuneRjo <rjo9000@outlook.com>
2023-11-19 03:31:18 -0500

    Add page handling for creating new chat

--- CipherApp.API/Pages/Chat/Create.cshtml.cs
+++ CipherApp.API/Pages/Chat/Create.cshtml.cs
@@ -1,14 +1,37 @@
+using CipherApp.BLL.Services.IServices;
+using CipherApp.DTO.Response;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace CipherApp.API.Pages.Chat
 {
     [Authorize]
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly IChatService _service;
+
+        public CreateModel(IChatService service)
         {
+            _service = service;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            try
+            {
+                string email = User.FindFirst(ClaimTypes.Email)?.Value;
+
+                ChatDto chat = await _service.CreateChatAsync(email);
+
+                return RedirectToPage("./Index");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
