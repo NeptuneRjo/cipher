bd9ac1f8a817a4a479ba6b0ffb445c910573fc9e
NeptuneRjo <rjo9000@outlook.com>
2023-11-29 17:09:23 -0500

    Remove unused code

--- CipherApp.DAL/Repositories/IRepositories/IMessageRepository.cs
+++ CipherApp.DAL/Repositories/IRepositories/IMessageRepository.cs
@@ -4,6 +4,5 @@ namespace CipherApp.DAL.Repositories.IRepositories
 {
     public interface IMessageRepository : IGenericRepository<Message>
     {
-        Task<Message> CreateAndAddToChatAsync(string chatUID, string content, int userId);
     }
 }
