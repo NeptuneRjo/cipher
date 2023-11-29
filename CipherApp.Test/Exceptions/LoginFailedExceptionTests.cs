a6b965d63bd9ec6930e835e344cdbd5ed2d21df8
NeptuneRjo <rjo9000@outlook.com>
2023-11-28 22:49:10 -0500

    Create unit tests for custom exceptions

---
+++ CipherApp.Test/Exceptions/LoginFailedExceptionTests.cs
@@ -0,0 +1,34 @@
+﻿using CipherApp.BLL.Utilities.CustomExceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherApp.Test.Exceptions
+{
+    public class LoginFailedExceptionTests
+    {
+        [Fact]
+        public void LoginFailedException_ThrowsException()
+        {
+            Assert.ThrowsAsync<LoginFailedException>(() => throw new LoginFailedException());
+        }
+
+        [Fact]
+        public void LoginFailedException_ThrowsException_WithMessage()
+        {
+            var ex = Assert.ThrowsAsync<LoginFailedException>(() => throw new LoginFailedException("Test"));
+
+            Assert.Contains("Test", ex.Result.Message);
+        }
+
+        [Fact]
+        public void LoginFailedException_ThrowsException_WithInnerException()
+        {
+            var ex = Assert.ThrowsAsync<LoginFailedException>(() => throw new LoginFailedException("Test", new Exception()));
+
+            Assert.IsType<Exception>(ex.Result.InnerException);
+        }
+    }
+}
---
+++ CipherApp.Test/Exceptions/NotFoundExceptionTests.cs
@@ -0,0 +1,34 @@
+﻿using CipherApp.BLL.Utilities.CustomExceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherApp.Test.Exceptions
+{
+    public class NotFoundExceptionTests
+    {
+        [Fact]
+        public void NotFoundException_ThrowsException()
+        {
+            Assert.ThrowsAsync<NotFoundException>(() => throw new NotFoundException());
+        }
+
+        [Fact]
+        public void NotFoundException_ThrowsException_WithMessage()
+        {
+            var ex = Assert.ThrowsAsync<NotFoundException>(()=> throw new NotFoundException("Test"));
+
+            Assert.Contains("Test", ex.Result.Message);
+        }
+
+        [Fact]
+        public void NotFoundException_ThrowsException_WithInnerException()
+        {
+            var ex = Assert.ThrowsAsync<NotFoundException>(() => throw new NotFoundException("Test", new Exception()));
+
+            Assert.IsType<Exception>(ex.Result.InnerException);
+        }
+    }
+}
